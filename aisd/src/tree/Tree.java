8ad90ef5c0543d2fe54fee1594d25517cd80a50c
RR <RR@Rafal.mshome.net>
2014-10-09 22:54:47 +0200

    full find method

--- aisd/src/tree/Tree.java
+++ aisd/src/tree/Tree.java
@@ -1,16 +1,24 @@
 package tree;
 
 public class Tree {
-	
+
 	private Node root;
-	
-	public Tree(){
+
+	public Tree() {
 		root = null;
 	}
-	
-	public Node find(int key){
+
+	public Node find(int key) {
 		Node current = root;
+		while (current.iData != key) {
+			if (key < current.iData)
+				current = current.leftChild;
+			else
+				current = current.rightChild;
+			if (current == null)
+				return null;
+		}
 		return current;
 	}
-	
+
 }
